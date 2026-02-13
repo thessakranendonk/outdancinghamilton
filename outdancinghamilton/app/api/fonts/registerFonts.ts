6ec4f70e414060f55a644df89192e41d3fb09434
Thessa <thessakranendonk@gmail.com>
2026-02-13 13:25:56 -0500

    update font in daily-image creation

--- outdancinghamilton/app/api/fonts/registerFonts.ts
+++ outdancinghamilton/app/api/fonts/registerFonts.ts
@@ -4,8 +4,8 @@ import { registerFont } from "canvas";
 import fs from "fs";
 
 export function registerFonts() {
-  const boldPath = path.join(process.cwd(), "app/api/fonts/ARIALBD.ttf");
-  const regularPath = path.join(process.cwd(), "app/api/fonts/Arial.ttf");
+  const boldPath = path.join(process.cwd(), "app/src/lib/fonts/ARIALBD.ttf");
+  const regularPath = path.join(process.cwd(), "app/src/lib/fonts/Arial.ttf");
 
   console.log("Bold font exists?", fs.existsSync(boldPath));
   console.log("Regular font exists?", fs.existsSync(regularPath));
